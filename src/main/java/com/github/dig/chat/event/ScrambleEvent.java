d82038cb592df4896c0dadd5ed99e3579d4aa179
Joseph <joseph42@live.co.uk>
2020-06-29 22:18:39 +0100

    Add messages

--- src/main/java/com/github/dig/chat/event/ScrambleEvent.java
+++ src/main/java/com/github/dig/chat/event/ScrambleEvent.java
@@ -3,18 +3,16 @@ package com.github.dig.chat.event;
 import com.github.dig.chat.ChatEvents;
 import lombok.extern.java.Log;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.List;
-import java.util.Optional;
-import java.util.Random;
-import java.util.UUID;
+import java.util.*;
 import java.util.logging.Level;
 
 @Log
@@ -23,9 +21,10 @@ public class ScrambleEvent implements BaseEvent {
     private static final String SCRAMBLE_KEY = "scramble";
     private static final Random RANDOM = new Random();
 
-    private ChatEvents chatEvents = ChatEvents.getInstance();
-    private ConfigurationSection config = chatEvents.getConfig().getConfigurationSection("events." + SCRAMBLE_KEY);
-    private String wordListName = config.getString("word-list", "scramble.txt");
+    private final ChatEvents chatEvents = ChatEvents.getInstance();
+    private final ConfigurationSection config = chatEvents.getConfig().getConfigurationSection("events." + SCRAMBLE_KEY);
+    private final ConfigurationSection msgConfig = chatEvents.getConfig().getConfigurationSection("messages." + SCRAMBLE_KEY);
+    private final String wordListName = config.getString("word-list", "scramble.txt");
 
     private Optional<UUID> winnerUUID;
     private String word;
@@ -49,6 +48,10 @@ public class ScrambleEvent implements BaseEvent {
         try {
             List<String> words = Files.readAllLines(wordList.toPath());
             word = words.get(RANDOM.nextInt(words.size()));
+
+            String shuffledWord = shuffle(word);
+            String announce = ChatColor.translateAlternateColorCodes('&', String.format(msgConfig.getString("announce", ""), shuffledWord));
+            Bukkit.broadcastMessage(announce);
         } catch (IOException e) {
             log.log(Level.SEVERE, "Unable to read word list for Scramble.", e);
         }
@@ -58,10 +61,28 @@ public class ScrambleEvent implements BaseEvent {
     public void stop() {
     }
 
+    private String shuffle(String value) {
+        List<Character> chars = new ArrayList<Character>(word.length());
+        for (char c : word.toCharArray()) {
+            chars.add(c);
+        }
+        Collections.shuffle(chars);
+        char[] shuffled = new char[chars.size()];
+        for (int i = 0; i < shuffled.length; i++) {
+            shuffled[i] = chars.get(i);
+        }
+        String shuffledWord = new String(shuffled);
+        return shuffledWord;
+    }
+
     @EventHandler
     public void onChat(AsyncPlayerChatEvent event) {
+        Player player = event.getPlayer();
         if (event.getMessage().equals(word) && !winnerUUID.isPresent()) {
-            winnerUUID = Optional.ofNullable(event.getPlayer().getUniqueId());
+            winnerUUID = Optional.ofNullable(player.getUniqueId());
+
+            String winner = ChatColor.translateAlternateColorCodes('&', String.format(msgConfig.getString("winner", ""), player.getName(), word));
+            Bukkit.broadcastMessage(winner);
         }
     }
 }
